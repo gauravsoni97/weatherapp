76392a74e8f63ce994b05e277eb349e5c08f3103
gaurav <gauravsoni8414@gmail.com>
2023-05-09 08:35:15 +0530

    ui set for right side

--- src/Components/RightSideUi/RightSide.jsx
+++ src/Components/RightSideUi/RightSide.jsx
@@ -3,34 +3,34 @@ import "./rightside.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 const RightSide = ({}) => {
-
-
-
   return (
     <div className="rightSide">
-      <div className="SearchSection">
-        <form >
-        
-        <input  className="searchBar" type="search" required autoComplete="off" />
-        <button className="searchButton" >
-          <SearchRoundedIcon />
-        </button>
-        </form>
-      </div>
+      <form>
+        <div className="SearchSection">
+          <input
+            className="searchBar"
+            type="search"
+            required
+            autoComplete="off"
+          />
+          <button className="searchButton">
+            <SearchRoundedIcon />
+          </button>
+        </div>
+      </form>
       <div className="weatherDetailsSection">
         <p>Weather Details</p>
 
         <div className="WeatherDetails">
           <div className="details_list">
-          <span>Cloudy</span>
-          <span>86%</span>
+            <span>Cloudy</span>
+            <span>86%</span>
           </div>
           <div className="details_list">
-          <span>Humidity</span>
-          <span>86%</span>
+            <span>Humidity</span>
+            <span>86%</span>
           </div>
         </div>
-
       </div>
     </div>
   );
