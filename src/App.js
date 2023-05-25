7f226d6632c0e7a77c3c8266dae6c6b5533132c1
gaurav <gauravsoni8414@gmail.com>
2023-05-25 20:55:46 +0530

    reset to custom search removed current location

--- src/App.js
+++ src/App.js
@@ -4,24 +4,18 @@ import RightSide from "./Components/RightSideUi/RightSide";
 import weatherBackground from "./Images/rain.jpg";
 
 const App = () => {
-  // ------- Current Location--------
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [city, setCity] = useState(null);
-  const [error, setError] = useState(null);
+  // ---------- Weather api integration ---------------
 
-  // ------------------------------------
-
-  const [cityWeather, setCityWeather] = useState("");
+  const [cityWeather, setCityWeather] = useState("sirsa");
   const [data, setData] = useState({
-    temp: 0,
-    pressure: 0,
-    humidity: 0,
+    temp: null,
+    pressure: null,
+    humidity: null,
     weather: "",
     icon: "",
-    windSpeed: 0,
-    lon: 0,
-    lat: 0,
+    windSpeed: null,
+    lon: null,
+    lat: null,
     city: "",
     state: "",
     country: "",
@@ -29,79 +23,31 @@ const App = () => {
 
   useEffect(() => {
     const weatherApi = async () => {
-      const response = await fetch(
-        `http://api.weatherstack.com/current?access_key=702dbdf22774e470cac8d592973f8a2f&query=${cityWeather}`
-      );
+     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityWeather}&units=metric&appid=5dc4d0db9fc5256a0ace471a19e48871
+     `
+      const response =
+        await fetch(apiUrl);
       const result = await response.json();
 
+      console.log("Weather Api Response", result);
+
       setData({
-        temp: result.current.temperature,
-        pressure: result.current.pressure,
-        humidity: result.current.humidity,
-        weather: result.current.weather_descriptions,
-        icon: result.weather_icons,
-        windSpeed: result.current.wind_speed,
-        lon: result.location.lon,
-        lat: result.location.lat,
-        city: result.location.name,
-        state: result.location.region,
-        country: result.location.country,
+        // temp: result.current.temperature,
+        // pressure: result.current.pressure,
+        // humidity: result.current.humidity,
+        // weather: result.current.weather_descriptions,
+        // icon: result.weather_icons,
+        // windSpeed: result.current.wind_speed,
+        // lon: result.location.lon,
+        // lat: result.location.lat,
+        // city: result.location.name,
+        // state: result.location.region,
+        // country: result.location.country,
       });
     };
 
     weatherApi();
   }, [cityWeather]);
-  useEffect(() => {
-    const getLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-          },
-          (error) => {
-            setError(error.message);
-          }
-        );
-      } else {
-        setError("Geolocation is not supported by your browser.");
-      }
-    };
-
-    getLocation();
-  }, []);
-
-  useEffect(() => {
-    const getCityName = async () => {
-      try {
-        const response = await fetch(
-          `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=10`
-        );
-        const data = await response.json();
-        if (response.ok) {
-          const currentCity =
-            data.address.city ||
-            data.address.town ||
-            data.address.village ||
-            data.address.hamlet ||
-            "Unknown";
-          setCity(currentCity);
-        } else {
-          setError("Unable to retrieve city name.");
-        }
-      } catch (error) {
-        setError("An error occurred while fetching the city name.");
-      }
-    };
-
-    if (latitude && longitude) {
-      getCityName();
-    }
-  }, [latitude, longitude]);
-
-  useEffect(() => {
-    setCityWeather(city);
-  }, []);
 
   return (
     <div
@@ -114,16 +60,8 @@ const App = () => {
     >
       <div className="blurLayer"></div>
       <div className="weatherapp_sections">
-        {latitude && longitude ? (
-          <>
-            <LeftSide data={data} weatherBackground={weatherBackground} />
-            <RightSide data={data} setCityWeather={setCityWeather} />
-          </>
-        ) : error ? (
-          <div>Error: {error}</div>
-        ) : (
-          <div>Loading...</div>
-        )}
+        <LeftSide data={data} weatherBackground={weatherBackground} />
+        <RightSide data={data} setCityWeather={setCityWeather} />
       </div>
     </div>
   );
